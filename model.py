6ea98e8b05996f22e5dfab0e75b17cbf64aae196
Rotten <thorbjoern.j@gmail.com>
2022-09-29 11:16:27 +0200

    Der blev lavet kode til hjulne og til tanken

--- model.py
+++ model.py
@@ -4,6 +4,15 @@ class Car(object):
     pass
 
 class Wheel(object):
+    def __int__(self):
+        self.orientaion = randint(0,360)
+
+    def rotate(self, revolutions):
+        degreesOfRotation = 360 * revolutions
+        self.orientaion = (self.orientaion + degreesOfRotation) %360
+
+
+
     pass
 
 class Engine(object):
@@ -13,4 +22,17 @@ class Gearbox(object):
     pass
 
 class Tank(object):
-    pass
+
+    def __int__(self):
+        self.capacity = 100
+        self.contents = 100
+
+    def refuel(self):
+        self.contents = self.capacity
+
+    def remove(self, amount):
+        self.contents = self.contents - amount
+        if self.contents < 0:
+            self.contents = 0
+
+        pass
