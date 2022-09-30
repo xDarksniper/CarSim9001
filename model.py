fc0e21069f898918d4d9807c3244180958b2dfb4
Rotten <thorbjoern.j@gmail.com>
2022-09-30 12:27:22 +0200

    Der blev lavet kode til hjulne og til tanken.n

--- model.py
+++ model.py
@@ -13,13 +13,25 @@ class Wheel(object):
 
 
 
-    pass
 
 class Engine(object):
     pass
 
 class Gearbox(object):
-    pass
+    def shiftUp(self):
+        pass
+
+    def ShiftDown(self):
+        pass
+
+    def __init__(self):
+        self.wheels = {}
+        for newWheel in ['frontLeft', 'frontRight','rearLeft', 'rearRight']:
+            self.wheels[newWheel] = Wheel()
+        self.gears = [0, 0.8, 1, 1.4, 2.2, 3.8]
+        self.clutchEngaged = False
+        self.currentGear = 0
+
 
 class Tank(object):
 
@@ -34,5 +46,3 @@ class Tank(object):
         self.contents = self.contents - amount
         if self.contents < 0:
             self.contents = 0
-
-        pass
